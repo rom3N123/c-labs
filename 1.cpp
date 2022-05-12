051061e0ca8fb9380c249891b7504b8dab8cab2b
Martynov <r.martynov@gk-adept.ru>
2022-05-12 10:03:47 +0300

    1: calculates result

--- 1.cpp
+++ 1.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include <math.h>
+
 using namespace std;
 
-// double calcFormula(double a, double b);
-// float calcFormula(float a, float b);
+double calcFormula(double a, double b);
+float calcFormula(float a, float b);
 
-// double calcFormula(double a, double b) {
-// 	return (pow(a - b, 3) - (pow(a, 3) - 3 * pow(a, 2) * b)) / pow(b, 3) - (3 * a * pow(b, 2));
-// }
-// float calcFormula(float a, float b) {
-// 	return (pow(a - b, 3) - (pow(a, 3) - 3 * pow(a, 2) * b)) / pow(b, 3) - (3 * a * pow(b, 2));
-// }
-
-// double calculate(double a, double b);
-// float calculate(float a,)
-
-double calculate(double a, double b) {
-	double result = 0;
-	result = (pow(a - b, 3) - (pow(a, 3) - 3 * pow(a, 2) * b)) / pow(b, 3) - (3 * a * pow(b, 2));
-	return result;
+double calcFormula(double a, double b) {
+	return (pow(a - b, 3) - (pow(a, 3) - 3 * pow(a, 2) * b)) / pow(b, 3) - (3 * a * pow(b, 2));
+}
+float calcFormula(float a, float b) {
+	return (pow(a - b, 3) - (pow(a, 3) - 3 * pow(a, 2) * b)) / pow(b, 3) - (3 * a * pow(b, 2));
 }
 
 int main() 
 {
-	double a = 1000;
-	double b = 0.0001;
-	double result = calculate(a, b);
-	cout << result << endl;
+	double doubleA = 1000;
+	double doubleB = 0.0001;
+	double doubleResult = calcFormula(doubleA, doubleB);
+
+	float floatA = 1000;
+	float floatB = 0.0001;
+	float floatResult = calcFormula(floatA, floatB);
+
+	cout << doubleResult << endl;
+	cout << floatResult << endl;
 }
 
