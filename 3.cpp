e0ccb28ed62c116c93e01c74df18228b69c38ae6
Martynov <r.martynov@gk-adept.ru>
2022-05-12 11:56:33 +0300

    make index get

--- 3.cpp
+++ 3.cpp
@@ -1,30 +1,55 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int* getArrayWithRandomNumbers(int length)
-{
-    int* arr = new int[length];
-
-	for(int i = 0; i < length; i++) {
-		arr[i] = rand() % 100;
-	}
- 
-    return arr;
-}
-
-int* filterArray(int* arr) {
-	int arrayLength = sizeof(arr) / sizeof(*arr);
-	int filteredArray[arrayLength] = 
-}
-
-int main() {
-	const int LENGTH = 10;
-
-	int* arr = getArrayWithRandomNumbers(LENGTH);
-
-	for(int i = 0; i < LENGTH; i++) {
+void outputVectorElements(vector<int> arr) {
+	for(int i = 0; i < arr.size(); i++) {
 		cout << arr[i];
 	}
+}
+
+vector<int> getArrayWithRandomNumbers(int length)
+{
+    vector<int> numbers(length);
+
+	for(int i = 0; i < length; i++) {
+		int randomNumber = rand() % 100;
+		numbers[i] = randomNumber;
+	}
+ 
+    return numbers;
+}
+
+vector<int> filterArray(vector<int> arr) {
+	vector<int> filtered;
+
+
+	copy_if(arr.begin(), arr.end(), back_inserter(filtered), [](int number) -> bool {
+		return number % 4 == 0;
+	});
+
+	return filtered;
+}
+
+vector<int> mapVectorAndAddZeroAfterNumber(vector<int> arr) {}
+
+int main() {
+	const int LENGTH = 100;
+
+	vector<int> numbers = getArrayWithRandomNumbers(LENGTH);
+
+	// outputVectorElements(numbers);
+
+	vector<int> filteredNumbers = filterArray(numbers);
+
+	outputVectorElements(filteredNumbers);
+
+	vector<int> transformed;
+
+
+	transform(filteredNumbers.begin(), filteredNumbers.end(), back_inserter(transformed), [](int number) {
+		return number + 1;
+	});
 }
