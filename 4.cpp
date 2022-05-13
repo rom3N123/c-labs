b506a968c8ee55d6712c5c65bcccbf583268b59e
Martynov <r.martynov@gk-adept.ru>
2022-05-13 21:10:18 +0300

    something wrong with 4.2

--- 4.cpp
+++ 4.cpp
@@ -15,6 +15,42 @@ int *getArray(int length) {
     return numbers;
 }
 
+int** createDeepArray(int rows, int cols) {
+	int **arr = new int* [rows];
+
+	for(int i = 0; i < rows; i++) {
+		arr[i] = getArray(cols);
+	}
+
+	return arr;
+}
+
+int** filterDeepArray(int **arr, int rows, int cols) {
+	int **newArr = new int*;
+	int resultCols = 0;
+
+	for(int row = 0; row < rows; row++) {
+		newArr[row] = new int;
+		int rowLength = 0;
+		for(int col = 0; col < cols; col++) {
+			if(col % 2 != 0) {
+				newArr[row][rowLength] = arr[row][col];
+				rowLength++;
+			}
+		}
+
+		resultCols = rowLength;
+	}
+
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < cols; j++) {
+			cout << newArr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+	return newArr;
+}
 
 tuple<int*, int> filterArray(int arr[], int length) {
 	int *filtered = new int;
@@ -38,11 +74,23 @@ void logArray(int arr[], int length) {
 }
 
 int main() {
-	int length = 10;
-	int *numbers = getArray(length);
+	// int length = 10;
+	// int *numbers = getArray(length);
 	
-	auto result = filterArray(numbers, length);
+	// tuple<int*, int> result = filterArray(numbers, length);
 
-	logArray(get<0>(result), get<1>(result));
-	delete [] numbers;
+	// delete[] numbers;
+
+	// int *resultArr = get<0>(result);
+	// int resultArrLength = get<1>(result);
+
+	// logArray(resultArr, resultArrLength);
+	int **arr = createDeepArray(5, 5);
+	int **filtered = filterDeepArray(arr, 5, 5);
+
+	// for(int i = 0; i < 5; i++) {
+	// 	cout << filtered[0][i] << endl;
+	// }
+
+	// delete[] resultArr;
 }
