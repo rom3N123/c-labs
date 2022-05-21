070af4a3dc1305231ecbbe337edab57eaac413b7
Martynov <r.martynov@gk-adept.ru>
2022-05-21 13:37:15 +0300

    feat: finish 4 lab

--- labs/4.cpp
+++ labs/4.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+void logDivider() {
+	cout << endl;
+	cout << "==================================" << endl;	
+}
+
 int *getArray(int length) {
     int *numbers = new int[length];
 
@@ -25,32 +30,32 @@ int** createDeepArray(int rows, int cols) {
 	return arr;
 }
 
-tuple<int**, int> filterDeepArray(int **arr, int rows, int cols) {
-	int **newArr = new int*;
-	int resultCols = 0;
+// tuple<int**, int> filterDeepArray(int **arr, int rows, int cols) {
+// 	int **newArr = new int*;
+// 	int resultCols = 0;
 
-	for(int row = 0; row < rows; row++) {
-		newArr[row] = new int;
-		int rowLength = 0;
-		for(int col = 0; col < cols; col++) {
-			if(col % 2 != 0) {
-				newArr[row][rowLength] = arr[row][col];
-				rowLength++;
-			}
-		}
+// 	for(int row = 0; row < rows; row++) {
+// 		newArr[row] = new int;
+// 		int rowLength = 0;
+// 		for(int col = 0; col < cols; col++) {
+// 			if(col % 2 != 0) {
+// 				newArr[row][rowLength] = arr[row][col];
+// 				rowLength++;
+// 			}
+// 		}
 
-		resultCols = rowLength + 1;
-	}
+// 		resultCols = rowLength + 1;
+// 	}
 
-	for(int i = 0; i < rows; i++) {
-		for(int j = 0; j < cols; j++) {
-			cout << newArr[i][j] << "\t";
-		}
-		cout << endl;
-	}
+// 	for(int i = 0; i < rows; i++) {
+// 		for(int j = 0; j < cols; j++) {
+// 			cout << newArr[i][j] << "\t";
+// 		}
+// 		cout << endl;
+// 	}
 
-	return { newArr , resultCols};
-}
+// 	return { newArr , resultCols};
+// }
 
 tuple<int*, int> filterArray(int arr[], int length) {
 	int *filtered = new int;
@@ -69,28 +74,92 @@ tuple<int*, int> filterArray(int arr[], int length) {
 
 void logArray(int arr[], int length) {
 	for(int i = 0; i < length; i++) {
-		cout << "Element: " << arr[i] << "\t" << endl;
+		cout << arr[i] << "\t";
 	}
+
+	logDivider();
+}
+
+void logDeepArray(int rows, int cols, int **arr) {
+	for(int r = 0; r < rows; r++) 
+	{
+		int *row = arr[r];
+
+		for(int c = 0; c < cols; c++) 
+		{
+			int column = row[c];
+
+			cout << column << "\t";
+		}
+
+		cout << endl;
+	}
+
+	logDivider();
+}
+
+int** addColumnAfterMax(int rows, int cols, int **arr) {
+	int **newArr = new int* [rows];
+
+	for(int r = 0; r < rows; r++) 
+	{
+		int *row = arr[r];
+
+		int maxCol = 0;
+		int maxColIndex;
+
+		for(int c = 0; c < cols; c++)
+		{
+			int column = row[c];
+
+			if(maxCol < column) 
+			{
+				maxCol = column;
+				maxColIndex = c;
+			}
+		}
+
+		bool hasInserted = false;
+		int *newRow = new int;
+
+		for(int c = 0; c < cols; c++) 
+		{
+			int column = row[c];
+
+			bool isMaxCol = c == maxColIndex;
+			int insertIndex = hasInserted ? c + 1 : c;
+
+			newRow[insertIndex] = column;
+
+			if(isMaxCol) 
+			{
+				hasInserted = true;
+				newRow[c + 1] = 1000;
+			}
+		}
+
+		newArr[r] = newRow;
+	}
+
+	return newArr;
 }
 
 int main() {
-	// int length = 10;
-	// int *numbers = getArray(length);
+	int length = 10;
+	int *numbers = getArray(length);
 	
-	// tuple<int*, int> result = filterArray(numbers, length);
+	logArray(numbers, length);
+	tuple<int*, int> result = filterArray(numbers, length);
+	delete[] numbers;
+	int *resultArr = get<0>(result);
+	int resultArrLength = get<1>(result);
+	logArray(resultArr, resultArrLength);
 
-	// delete[] numbers;
-
-	// int *resultArr = get<0>(result);
-	// int resultArrLength = get<1>(result);
-
-	// logArray(resultArr, resultArrLength);
-	int **arr = createDeepArray(5, 5);
-	auto result = filterDeepArray(arr, 5, 5);
-
-	// for(int i = 0; i < 5; i++) {
-	// 	cout << filtered[0][i] << endl;
-	// }
-
-	// delete[] resultArr;
+	int r = 5, c = 5;
+	int **arr = createDeepArray(r, c);
+	logDeepArray(r, c, arr);
+	int **transformedArr = addColumnAfterMax(r, c, arr);
+	delete[] arr;
+	logDeepArray(r, c + 1, transformedArr);
+	delete[] transformedArr;
 }
