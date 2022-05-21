1f6218dcda90178932d730f313eca16116d9673e
Martynov <r.martynov@gk-adept.ru>
2022-05-21 13:39:28 +0300

    feat: make second lab

--- labs/2.cpp
+++ labs/2.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main() {
+int main()
+{
+	double eps=0.001;
 
+
+	double sum = 0;
+	double a = 1;
+	double input;
+
+	for (int n = 1;; n++)
+	{
+		a*=(double)4*n/((4*n)*(4*n+1)*(4*n+2)*(4*n+3));
+
+		if (abs(a) < eps) {
+			break;
+		}
+		sum += a;
+	}
+
+	cout << "Sum series: " << sum << endl;
+	
+	cout << endl;
+	
+	return 0;
 }
